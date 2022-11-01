dcfcb4394859c41a4f3f3513e56f19515321d11f
sohy19 <sohy19@hufs.ac.kr>
2022-11-01 23:19:18 +0900

    fix: add Outlet

--- src/components/home/header/index.jsx
+++ src/components/home/header/index.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import { Logo, HeaderItem, LogOut, Profile, Wrap, HeaderItems } from "./styled";
+import { Outlet } from "react-router-dom";
 
 const Header = () => {
 	return (
-		<Wrap>
-			<Logo>Logo</Logo>
-			<HeaderItems>
-				<HeaderItem>Item one</HeaderItem>
-				<HeaderItem>Item two</HeaderItem>
-				<HeaderItem>Item three</HeaderItem>
-			</HeaderItems>
-			<LogOut>LOGOUT</LogOut>
-			<Profile>profile</Profile>
-		</Wrap>
+		<>
+			<Wrap>
+				<Logo>Logo</Logo>
+				<HeaderItems>
+					<HeaderItem>Item one</HeaderItem>
+					<HeaderItem>Item two</HeaderItem>
+					<HeaderItem>Item three</HeaderItem>
+				</HeaderItems>
+				<LogOut>LOGOUT</LogOut>
+				<Profile>profile</Profile>
+			</Wrap>
+			<Outlet />
+		</>
 	);
 };
 
