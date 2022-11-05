6fdebbdce6e6778185a8df609e10bb2eaeca746e
JunAhJang <junga5853@naver.com>
2022-11-05 17:29:58 +0900

    feat: Login style 요소 추가

--- src/components/auth/login/index.jsx
+++ src/components/auth/login/index.jsx
@@ -1,10 +1,35 @@
 import React from "react";
-import { LoginSection, LoginLeft } from "./styled";
+import {
+  LoginSection,
+  LoginLeft,
+  LeftTitle,
+  LeftLogo,
+  LoginRight,
+  RightTitle,
+  RightSubTitle,
+  LoginInput,
+  LoginButton,
+  LoginLink,
+} from "./styled";
 
 const Login = () => {
   return (
     <LoginSection>
-      <LoginLeft />
+      <LoginLeft>
+        <LeftTitle>
+          The Best Fit <br />
+          for My Plant
+        </LeftTitle>
+        <LeftLogo>Logo</LeftLogo>
+      </LoginLeft>
+      <LoginRight>
+        <RightTitle>KLAE 에 오신 것을 환영합니다!</RightTitle>
+        <RightSubTitle>지금 로그인하고 반려식물을 확인해보세요.</RightSubTitle>
+        <LoginInput />
+        <LoginInput />
+        <LoginButton>LOGIN</LoginButton>
+        <LoginLink>계정이 없으신가요? 지금 회원가입하세요!</LoginLink>
+      </LoginRight>
     </LoginSection>
   );
 };
--- src/components/auth/login/styled.js
+++ src/components/auth/login/styled.js
@@ -2,7 +2,8 @@ import styled from "styled-components";
 import Img from "../../../assets/flower_login.jpg";
 
 export const LoginSection = styled.div`
-  background-color: red;
+  background-color: E5E5E5;
+  display: flex;
 `;
 
 export const LoginLeft = styled.div`
@@ -10,6 +11,75 @@ export const LoginLeft = styled.div`
   width: 470px;
   height: 900px;
   color: white;
-  margin: 0;
-  padding: 0;
+`;
+
+export const LeftTitle = styled.div`
+  // background-color: black;
+  font-size: 22pt;
+  font-weight: 800;
+  color: white;
+  width: 180px;
+  height: 40px;
+  margin: 0 auto;
+  white-space: pre-line;
+  position: relative;
+  top: 600px;
+`;
+
+export const LeftLogo = styled.div`
+  width: 100px;
+  hegith: 100px;
+  margin: 0 auto;
+  position: relative;
+  top: 150px;
+  color: white;
+  font-size: 20pt;
+`;
+
+export const LoginRight = styled.div`
+  //background-color: green;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+export const RightTitle = styled.div`
+  //background-color: red;
+  color: rgba(33, 33, 33, 1);
+  font-size: 15pt;
+  width: 290px;
+  hegiht: 45px;
+  padding-bottom: 20px;
+  text-align: center;
+`;
+
+export const RightSubTitle = styled.div`
+  //background-color: blue;
+  color: rgba(66, 66, 66, 1);
+  font-size: 10pt;
+  padding-bottom: 25px;
+  text-align: center;
+`;
+
+export const LoginInput = styled.input`
+  width: 420px;
+  height: 70px;
+  border: 1px solid rgba(117, 117, 117, 1);
+  margin-bottom: 20px;
+`;
+
+export const LoginButton = styled.button`
+  background-color: rgba(36, 62, 51, 1);
+  width: 420px;
+  height: 70px;
+  color: white;
+  margin-bottom: 20px;
+`;
+
+export const LoginLink = styled.div`
+  text-decoration: underline;
+  font-size: 8pt;
+  margin-bottom: 20px;
 `;
