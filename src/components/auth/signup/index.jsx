e362d6c3550c2a0b73556a898b2ff4694e40aaf8
JunAhJang <junga5853@naver.com>
2022-11-13 00:01:28 +0900

    feat: 회원가입  페이지 CSS 대강 완료

--- src/components/auth/signup/index.jsx
+++ src/components/auth/signup/index.jsx
@@ -4,6 +4,13 @@ import {
   SignUpSection,
   SignLeftTitle,
   SignLeftSubTitle,
+  SignRight,
+  SignRightTitle,
+  SignRightForm,
+  SignRigntFormTitle,
+  SignRigntFormInput,
+  SignRigntFormbutton,
+  SignLoginLink,
 } from "./styled";
 
 const Signup = () => {
@@ -22,6 +29,24 @@ const Signup = () => {
           식물 집사들을 위한 서비스, KLAE.
         </SignLeftSubTitle>
       </SignUpLeft>
+      <SignRight>
+        <SignRightTitle>
+          지금 KLAE 에 가입하고 <br />
+          맞춤형 식물을 추천받으세요!
+        </SignRightTitle>
+        <SignRightForm>
+          <SignRigntFormTitle>Username</SignRigntFormTitle>
+          <SignRigntFormInput />
+          <SignRigntFormTitle>Email</SignRigntFormTitle>
+          <SignRigntFormInput />
+          <SignRigntFormTitle>Password</SignRigntFormTitle>
+          <SignRigntFormInput type="password" />
+          <SignRigntFormbutton>SIGN UP</SignRigntFormbutton>
+        </SignRightForm>
+        <SignLoginLink>
+          계정이 있으신가요? 지금 바로 로그인 하세요.
+        </SignLoginLink>
+      </SignRight>
     </SignUpSection>
   );
 };
--- src/components/auth/signup/styled.js
+++ src/components/auth/signup/styled.js
@@ -40,3 +40,70 @@ export const SignLeftSubTitle = styled.div`
   top: 70vh;
   text-align: center;
 `;
+
+export const SignRight = styled.div`
+  //background-color: green;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 50vw;
+  height: 100vh;
+  margin: -8px;
+`;
+
+export const SignRightTitle = styled.div`
+  //background-color: red;
+  font-size: 2.5vw;
+  font-weight: 900;
+  hite-space: pre-line; //공백처리
+  color: rgba(36, 36, 36, 1);
+  text-align: center;
+  padding-bottom: 4vh;
+`;
+
+export const SignRightForm = styled.div`
+  //background-color: blue;
+  width: 30vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const SignRigntFormTitle = styled.div`
+  //background-color: pink;
+  color: rgba(117, 117, 117, 1);
+  font-size: 1vw;
+  font-weight: 500;
+  width: 30vw;
+`;
+
+export const SignRigntFormInput = styled.input`
+  all: unset;
+  //background-color: red;
+  border-bottom: 1px solid rgba(117, 117, 117, 1);
+  width: 30vw;
+  height: 4vh;
+  margin-bottom: 4vh;
+`;
+
+export const SignRigntFormbutton = styled.button`
+  all: unset;
+  background-color: rgba(109, 177, 120, 1);
+  width: 18vw;
+  height: 8vh;
+  color: white;
+  margin-bottom: 2.5vh;
+  cursor: pointer;
+  text-align: center;
+  border-radius: 50px;
+  box-shadow: 0 4px 4px 1px rgba(0, 0, 0, 0.25); //X Y 흐릿 크기 색
+`;
+
+export const SignLoginLink = styled.div`
+  text-decoration: underline;
+  font-size: 1vw;
+  font-weight: 800;
+  margin-bottom: 2.5vh;
+  cursor: pointer;
+`;
