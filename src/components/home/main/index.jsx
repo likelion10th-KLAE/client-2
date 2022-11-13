391d2838fc5512ff716ed3d85876205c7f6c96ce
thwjdwjs <102451051+thwjdwjs@users.noreply.github.com>
2022-11-13 21:45:16 +0900

    Update index.jsx
    디자인 틀만 완성됨,,,

--- src/components/home/main/index.jsx
+++ src/components/home/main/index.jsx
@@ -1,7 +1,76 @@
 import React from "react";
+import {
+	Sidebar,
+	Userinfo,
+	Userpic,
+	Usernick,
+	Sidelist,
+	Listcompo,
+	Sideplantpic,
+	Sideplantname,
+	Listcompoclicked,
+	Sideplantpicclicked,
+	Sideplantnameclicked,
+	Infoeditbtn,
+	EditImg,
+	EditTxt,
+	Plantname,
+	Plantdday,
+	LogPage,
+	LogListsec,
+	LogList,
+	Logplantimg,
+	Logtitle,
+	Logdate,
+	Page
+} from "./styled";
 
 const Main = () => {
-	return <div></div>;
+	return <div>
+		<Page>
+		</Page>
+		<Sidebar>
+			<Userpic/>
+			<Usernick>(닉네임) 님</Usernick>
+			<Sidelist>
+				<Listcompo>
+					<Sideplantpic/><Sideplantname>식물 1</Sideplantname>
+					</Listcompo>
+				<Listcompoclicked>
+					<Sideplantpicclicked/>
+					<Sideplantnameclicked>식물 2</Sideplantnameclicked>
+				</Listcompoclicked>
+			</Sidelist>
+		</Sidebar>
+	
+		<Plantname>식물 이름</Plantname>
+		<Plantdday>키운지 D+nnn일 째</Plantdday>
+
+		<Infoeditbtn>
+			<EditImg/>
+			<EditTxt>식물 일지 추가하기</EditTxt>
+		</Infoeditbtn>
+
+		<LogPage/>
+		<LogListsec>
+			<LogList>
+				<Logplantimg/>
+				<Logtitle>일지 제목</Logtitle>
+				<Logdate>2022.06.25</Logdate>
+			</LogList>
+			<LogList>
+				<Logplantimg/>
+				<Logtitle>일지 제목</Logtitle>
+				<Logdate>2022.06.25</Logdate>
+			</LogList>
+			<LogList>
+				<Logplantimg/>
+				<Logtitle>일지 제목</Logtitle>
+				<Logdate>2022.06.25</Logdate>
+			</LogList>
+		</LogListsec>
+		
+		</div>;
 };
 
 export default Main;
