98d16247b3792399faa47e27ca4fcb10a2a8db52
JunAhJang <junga5853@naver.com>
2022-11-28 13:49:02 +0900

    feat: initial 페이지 css

--- src/components/home/initial/index.jsx
+++ src/components/home/initial/index.jsx
@@ -1,7 +1,88 @@
 import React from "react";
+import {
+  PersonIcon,
+  ButtonContainer,
+  InitialBottom,
+  InitialBottomContainer,
+  InitialItemCircle,
+  InitialItemBox,
+  InitialSection,
+  InitialTop,
+  InitialTopBanner,
+  InitialTopButton,
+  InitialTopSubTitle,
+  InitialTopTitle,
+  InitialItemTitle,
+  InitialItemText,
+  ListIcon,
+  StarIcon,
+} from "./styled";
+import { Link } from "react-router-dom";
 
 const Initial = () => {
-	return <div></div>;
+  return (
+    <InitialSection>
+      <InitialTop>
+        <InitialTopTitle>반려식물 일지를 기록하세요</InitialTopTitle>
+        <InitialTopSubTitle>식물과 함께하는 삶</InitialTopSubTitle>
+        <InitialTopBanner />
+        <ButtonContainer>
+          <Link
+            to="/signup"
+            style={{ textDecoration: "none", color: "inherit" }}
+          >
+            <InitialTopButton>회원가입</InitialTopButton>
+          </Link>
+          <Link
+            to="/login"
+            style={{ textDecoration: "none", color: "inherit" }}
+          >
+            <InitialTopButton>로그인</InitialTopButton>
+          </Link>
+        </ButtonContainer>
+      </InitialTop>
+      <InitialBottom>
+        <InitialBottomContainer>
+          <InitialItemBox>
+            <InitialItemCircle>
+              <PersonIcon />
+            </InitialItemCircle>
+            <InitialItemTitle>사람들과 소통하세요.</InitialItemTitle>
+            <InitialItemText>
+              Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis
+              ullamco cillum dolor. Voluptate exercitation incididunt aliquip
+              deserunt reprehenderit elit laborum.Voluptate exercitation
+              incididunt aliquip
+            </InitialItemText>
+          </InitialItemBox>
+          <InitialItemBox>
+            <InitialItemCircle>
+              <ListIcon />
+            </InitialItemCircle>
+            <InitialItemTitle>식물일지를 작성하세요.</InitialItemTitle>
+            <InitialItemText>
+              Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis
+              ullamco cillum dolor. Voluptate exercitation incididunt aliquip
+              deserunt reprehenderit elit laborum.Voluptate exercitation
+              incididunt aliquip
+            </InitialItemText>
+          </InitialItemBox>
+          <InitialItemBox>
+            <InitialItemCircle>
+              <StarIcon />
+            </InitialItemCircle>
+            <InitialItemTitle>식물을 추천받으세요.</InitialItemTitle>
+            <InitialItemText>
+              Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis
+              ullamco cillum dolor. Voluptate exercitation incididunt aliquip
+              deserunt reprehenderit elit laborum.Voluptate exercitation
+              incididunt aliquip
+            </InitialItemText>
+          </InitialItemBox>
+        </InitialBottomContainer>
+      </InitialBottom>
+    </InitialSection>
+  );
 };
 
 export default Initial;
---
+++ src/components/home/initial/styled.js
@@ -0,0 +1,135 @@
+import styled from "styled-components";
+import BannerImg from "../../../assets/initial/initial_banner.png";
+import PersonIcn from "../../../assets/initial/person.png";
+import ListIcn from "../../../assets/initial/list.png";
+import StarIcn from "../../../assets/initial/star.png";
+
+export const InitialSection = styled.div`
+  background-color: white;
+`;
+
+export const InitialTop = styled.div`
+  background-color: rgba(228, 228, 228, 1);
+  width: 100vw;
+  height: 606px; //40vh;
+  margin: -8px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+`;
+
+export const InitialTopBanner = styled.div`
+  background-image: url(${BannerImg});
+  background-repeat: no-repeat;
+  background-size: cover;
+  width: 1188px; //90vw;
+  height: 287px; // 35vh;
+  position: absolute;
+  bottom: 0px;
+`;
+
+export const InitialTopTitle = styled.div`
+  font-size: 50px;
+  color: rgba(87, 103, 91, 1);
+  margin-top: 50px;
+`;
+
+export const InitialTopSubTitle = styled.div`
+  color: white;
+  font-size: 32px;
+  margin-top: 30px;
+`;
+
+export const ButtonContainer = styled.div`
+  //background-color: pink;
+  width: 210px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const InitialTopButton = styled.button`
+  all: unset;
+  background-color: rgba(134, 161, 116, 1);
+  color: white;
+  font-size: 14px;
+  cursor: pointer;
+  width: 100px;
+  height: 54px;
+  border-radius: 4px;
+  text-align: center;
+  margin-top: 40px;
+`;
+
+export const InitialBottom = styled.div`
+  //background-color: black;
+  width: 100vw;
+  height: 786px; //60vh
+  margin: -8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const InitialBottomContainer = styled.div`
+  background-color: white;
+  width: 1296px;
+  height: 297px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const InitialItemBox = styled.div`
+  //background-color: pink;
+  width: 416px;
+  height: 297px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const InitialItemTitle = styled.div`
+  font-size: 24px;
+  color: rgba(44, 44, 44, 1);
+`;
+
+export const InitialItemText = styled.div`
+  font-size: 16px;
+  color: rgba(44, 44, 44, 1);
+  text-align: center;
+`;
+
+export const InitialItemCircle = styled.div`
+  border-radius: 70px;
+  border: 1px solid rgba(148, 195, 147, 1);
+  width: 108px;
+  height: 108px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const PersonIcon = styled.div`
+  background-image: url(${PersonIcn});
+  background-repeat: no-repeat;
+  background-size: cover;
+  width: 32px;
+  height: 32px;
+`;
+
+export const ListIcon = styled.div`
+  background-image: url(${ListIcn});
+  background-repeat: no-repeat;
+  background-size: cover;
+  width: 32px;
+  height: 32px;
+`;
+export const StarIcon = styled.div`
+  background-image: url(${StarIcn});
+  background-repeat: no-repeat;
+  background-size: cover;
+  width: 32px;
+  height: 32px;
+`;
