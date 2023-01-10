12cc3ca6fcfe7b8f3ddbcaa856ead8cb623f58fe
thwjdwjs <102451051+thwjdwjs@users.noreply.github.com>
2023-01-10 17:31:52 +0900

    feat: api 연동 준비

--- src/components/home/main/lognone.jsx
+++ src/components/home/main/lognone.jsx
@@ -1,9 +1,6 @@
-/* 작성된 일지가 없는 경우 페이지입니당,,, 일단 로직 생각안해서 새로 페이지 파서 했는데 괜찮을까유,,? */
-/* App.js에 코드 추가 : <Route path="plant" element={<Lognone />} /> (일지 없을 경우 메인 페이지) */
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
 	Sidebar,
-	Userpic,
 	Usernick,
 	Page,
 	Text1,
@@ -13,18 +10,66 @@ import {
 	Registerbtn
 } from "./styled";
 import { Link } from "react-router-dom";
-//import {render} from "react-dom";
-//import { BrowserRouter } from 'react-router-dom';
-//import { Routes } from 'react-router-dom';
-//import { Route } from 'react-router-dom';
-//import Recommend from './styled'
+import axios from 'axios';
+import Usericon from "../../../assets/diary/유저 정보란.png";
+
 
 const Lognone = () => {
+	const [user, setUser] = useState(null);
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState(null);
+	const [imageSrc, setImageSrc] = useState(Usericon);
+/*
+	useEffect(() => {
+	  const fetchUsers = async () => {
+		try {
+		  setError(null);
+		  setUser(null);
+		  setLoading(true);
+		  const response = await axios.get(
+			"http://ec2-3-39-207-4.ap-northeast-2.compute.amazonaws.com/account/mypage/"
+		  );
+		  setUser(response.data); 
+		} catch (e) {
+		  setError(e);
+		}
+		setLoading(false);
+	  };
+  
+	  fetchUsers();
+	}, []);
+  
+	if (loading) return <div>로딩중..</div>;
+	if (error) return <div>에러가 발생했습니다</div>;
+	if (!user) return null;
+	*/
+	const accountMypage = 
+	{
+		"email":"klae@gmail.com",
+		"password":"pbkdf2_sha256$390000$m9CtGlBmyIvkCL341Yhfjo$9aCy0HzvKhde8lGZQHv6uxfLCyKhr78hY4O2L8gkS5w=",
+		"username":"klae",
+		"profile_image":null
+	}
+
+	const imagestyle ={
+		position: 'absolute',
+		width: '2.57vw',
+	    height: '4.22vh',
+	    left: '2.85vw',
+    	top: '4vh',
+    	'background-repeat': 'no-repeat',
+    	'background-size': 'contain'
+	}
+
 	return <div>
 		<Page/>
 		<Sidebar>
-        <Userpic/>
-			<Usernick>(닉네임) 님</Usernick>
+       		<img 
+				src={imageSrc} 
+				alt="preview-img" 
+				style={imagestyle} 
+			/>
+			<Usernick> {accountMypage.username} 님</Usernick>
         </Sidebar>
 			<Text1>
 				일지가 비어있어요! <br/>
@@ -41,7 +86,7 @@ const Lognone = () => {
 			<Text3>
 				이미 기르고 있는 식물이 있다면 KLAE에 등록하세요.
 			</Text3>
-			<Link to="/plant/addplant">
+			<Link to="/plant/addplant/createplant">
 				<Registerbtn>
 				식물 등록 하기
 				</Registerbtn>
